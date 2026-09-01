ee61d5cd674fb646271802efd4ef0d1f85ef3509
rashmisahoo-developer <rashmisahoo0108@gmail.com>
2026-09-01 16:43:39 +0000

    Add terraform basic command

---
+++ tf-basic-command-run/azurerm_resource_group.tf
@@ -0,0 +1,24 @@
+# Terraform Settings Block
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.0" # Optional but recommended in production
+    }    
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create Resource Group 
+resource "azurerm_resource_group" "my_demo_rg1" {
+  location = "eastus"
+  name = "my-demo-rg1"  
+}
+
+#terraform init, terraform plan, terraform apply (terraform.tfstate) is created)
+##terraform destroy (terraform.tfstate is deleted)
